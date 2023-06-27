4b0ba0c9372461109c258bc3ddff84ba13269a0f
DFranck <franckdufournet@hotmail.fr>
2023-06-27 11:04:45 +0700

    nouveaux projet3 + test lienProjet

--- assets/js/index.js
+++ assets/js/index.js
@@ -62,8 +62,18 @@ links.forEach((link) => {
 });
 
 //----------------------------SECTION PROJETS-------------------------------------
-
+// https://dfranck.github.io/Formation-OpenClassroom-Projet3/
 const projets = [
+  {
+    titre: "ohmyfood",
+    image: "./assets/img/ohmyfood.png",
+    description: `ohmyfood, est l'intégration du projet 3 de la formation d'OpenClassroom. Il consiste en l'intégration d'une maquette en un site web en utilisant uniquement HTML et CSS (SASS autorisé). Voici un résumé de ce projet :
+
+    Objectif : L'objectif principal du projet 3 est de de coder en "Mobile first"et de versionner le code sur GitHub afin de le rendre accessible sur GitHub Pages.`,
+    technologies: ["HTML", "CSS"],
+    lienGitHub: "https://github.com/DFranck/Formation-OpenClassroom-Projet3",
+    lienProjet: "https://dfranck.github.io/Formation-OpenClassroom-Projet3/",
+  },
   {
     titre: "Booki",
     image: "./assets/img/Booki.png",
