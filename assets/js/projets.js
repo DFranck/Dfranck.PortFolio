b6546bfd9d1b1fcc9dccd3bef0b3ab0fa0704498
DFranck <franckdufournet@hotmail.fr>
2023-07-04 15:56:56 +0700

    probleme cards

--- assets/js/projets.js
+++ assets/js/projets.js
@@ -11,30 +11,25 @@ fetch("assets/json/projetsFormations.json")
     projetsFormations.push(...data);
     projetsFormations.forEach((projet) => {
       const html = `
-      <div class="projet">
+      <div class="card">
       <h3 class="projet-titre">${projet.titre}</h3>
-      <div class=img-container>
       <img src="${projet.image}" alt="${projet.titre}" class="projet-image">
-      </div>
-          <div class="description-container">
-          
-          <h4>Technologies utilisées:</h4>
-          <ul class="projet-technologies"> 
-          ${projet.technologies.map((tech) => `<li>${tech}</li>`).join("")}
-          </ul>
-          <h5>${
-            projet.lienGitHub
-              ? `<a href="${projet.lienGitHub}" class="projet-lien" target="_blank"><i class="fa-brands fa-github" id="github"></i>Code Source</a>`
-              : ""
-          }</h5>
+      <h4>Technologies utilisées:</h4>
+      <ul class="projet-technologies"> 
+      ${projet.technologies.map((tech) => `<li>${tech}</li>`).join("")}
+      </ul>
+      <h5>${
+        projet.lienGitHub
+          ? `<a href="${projet.lienGitHub}" class="projet-lien" target="_blank"><i class="fa-brands fa-github" id="github"></i>Code Source</a>`
+          : ""
+      }</h5>
           <h5>${
             projet.lienProjet
               ? `<a href="${projet.lienProjet}" class="projet-lien" target="_blank"><i class="fa-solid fa-globe"></i>Version déployée</a>`
               : ""
           }</h5>
           </div>
-          </div>
-      `;
+          `;
       sectionProjets.innerHTML += html;
     });
   });
