fe15e5ce6356d8ec1c3104c6b3db616a0bf9e6d0
DFranck <franckdufournet@hotmail.fr>
2023-07-05 11:54:38 +0700

    Cards avec description ok

--- assets/js/projets.js
+++ assets/js/projets.js
@@ -12,12 +12,9 @@ fetch("assets/json/projetsFormations.json")
     projetsFormations.forEach((projet) => {
       const html = `
       <div class="card">
-      <h3 class="projet-titre">${projet.titre}</h3>
       <img src="${projet.image}" alt="${projet.titre}" class="projet-image">
-      <h4>Technologies utilisées:</h4>
-      <ul class="projet-technologies"> 
-      ${projet.technologies.map((tech) => `<li>${tech}</li>`).join("")}
-      </ul>
+      <div class="description">
+      <p>${projet.description}</p>
       <h5>${
         projet.lienGitHub
           ? `<a href="${projet.lienGitHub}" class="projet-lien" target="_blank"><i class="fa-brands fa-github" id="github"></i>Code Source</a>`
@@ -28,7 +25,17 @@ fetch("assets/json/projetsFormations.json")
               ? `<a href="${projet.lienProjet}" class="projet-lien" target="_blank"><i class="fa-solid fa-globe"></i>Version déployée</a>`
               : ""
           }</h5>
+      </div>
+      <h3 class="projet-titre">${projet.titre}</h3>
+      <div class="mini-description">
+      <h4>Technologies utilisées:</h4>
+      <ul class="projet-technologies"> 
+      ${projet.technologies.map((tech) => `<li>${tech}</li>`).join("")}
+      </ul>
+      
           </div>
+          </div>
+
           `;
       sectionProjets.innerHTML += html;
     });
